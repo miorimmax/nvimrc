63cb340b8094877fb057719af2faf8b22a2ee4c7
Max Miorim <max.miorim@nubank.com.br>
2017-08-02 15:57:53 -0300

    Improve install script

--- install.sh
+++ install.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 set -euf pipefail
-brew install neovim
+brew install neovim --HEAD
 
-pip2 install --upgrade neovim
-pip3 install --upgrade neovim
+type pip2 >/dev/null 2>&1 && pip2 install --upgrade neovim
+type pip3 >/dev/null 2>&1 && pip3 install --upgrade neovim
 
-for version in $(rbenv versions --bare --skip-aliases); do
-  export RBENV_VERSION=$version
+if type rbenv >/dev/null 2>&1; then
+  for version in $(rbenv versions --bare --skip-aliases); do
+    export RBENV_VERSION=$version
 
-  if gem list --local | grep -q neovim; then
-    gem update --no-document neovim
-  else
-    gem install --no-document neovim
-  fi
+    if gem list --local | grep -q neovim; then
+      gem update --no-document neovim
+    else
+      gem install --no-document neovim
+    fi
 
-  rbenv rehash
-done
+    rbenv rehash
+  done
 
-unset RBENV_VERSION
+  unset RBENV_VERSION
+fi
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl --create-dirs -fLo ~/.local/share/nvim/site/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim -c 'let g:plug_window=""' \
+nvim --headless \
+  -c 'let g:plug_window=""' \
   +PlugClean! \
   +PlugInstall! \
   +PlugUpdate! \
