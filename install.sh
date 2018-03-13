5e67a38be8683c2405fbadbb3964357bdca9b9b6
Max Miorim <max.miorim@nubank.com.br>
2018-03-12 21:20:59 -0300

    Install language bindings before installing plugins

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -euf pipefail
 
+type pip2 && pip2 install --upgrade neovim
+type pip3 && pip3 install --upgrade neovim
+type gem && gem install --no-rdoc --no-ri neovim
+type npm && npm install --global neovim
+
 curl --create-dirs -fLo ~/.local/share/nvim/site/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
